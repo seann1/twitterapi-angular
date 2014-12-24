92320fef6508bb74abd80e8f369218350e8dc41e
Sean <seanniesen@gmail.com>
2014-12-23 17:30:31 -0800

    working on updating api call

--- twitter.js
+++ twitter.js
@@ -1,8 +1,15 @@
 angular.module('Twitter', ['ngResource']);
 
+function twitterSearch(result) {
+	alert('this is ok');
+}
+
 function TwitterCtrl($scope, $resource) {
-	$scope.twitter = $resource('http://search.twitter.com/:action',
-		{action:'search.json', q:'angularjs'});
-	$scope.twitter.get();
+	$scope.twitter = $resource('https://api.twitter.com/1.1/search/tweets.json',
+		{action:'search.json', q:'#CocaCola', callback: 'JSON_CALLBACK'},
+		{get:{method: "JSONP"}});
+	$scope.twitterResult = $scope.twitter.get(function(result){
+		alert("this is better");
+	});
 
 }
